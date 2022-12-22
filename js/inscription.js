5764d330ac60512a6238fbaa68410d101e2c7eda
Andy <schne.andy@gmail.com>
2022-12-22 14:13:38 +0100

    modifications_ajout_sass_ajout_javascript_22_12

---
+++ js/inscription.js
@@ -0,0 +1,29 @@
+const btnSerious = document.querySelector(".proj-experience__serious"); 
+const btnFun = document.querySelector(".proj-experience__fun"); 
+const btnBeer = document.querySelector(".proj-experience__beer"); 
+const btnBeef = document.querySelector(".proj-experience__beef"); 
+
+console.log('wesh');
+
+btnSerious.addEventListener('click', ()=> {
+    console.log('wesh');
+    console.log(btnSerious);
+    btnSerious.style.background = "red";
+})
+
+
+btnFun.addEventListener('click', ()=> {
+    console.log('wesh');
+    console.log(btnFun);
+})
+
+
+btnBeer.addEventListener('click', ()=> {
+    console.log('wesh');
+    console.log(btnBeer);
+})
+
+btnBeef.addEventListener('click', ()=> {
+    console.log('wesh');
+    console.log(btnBeef);
+})
